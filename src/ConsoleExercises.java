aa2bf49153a0f21c89e728d8c1d32f346e6ef9a7
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-10 09:27:34 -0600

    forgot to commit these

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class ConsoleExercises {
+    public static void main(String[] args) {
+//        double pi = 3.14159;
+//        System.out.format("The value of pi is approximately %.2f \n",pi);
+//
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Your integer input will be saved in a variable.");
+//        int userInt = sc.nextInt();
+//        System.out.printf("Your integer is %d",userInt);
+//
+//        Scanner sb = new Scanner(System.in);
+//        System.out.println("Enter 3 words");
+//        String one = sb.next();
+//        String two = sb.next();
+//        String three = sb.next();
+//        System.out.println(one);
+//        System.out.println(two);
+//        System.out.println(three);
+//        System.out.printf("You entered %s %s %s",one,two,three);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Where is the Dutchman's Treasure?");
+//        String input = sc.nextLine();
+//        System.out.printf("%s? He pantomimed sarcastically.",input);
+
+        Scanner sa = new Scanner(System.in);
+        sa.useDelimiter("\n");
+        System.out.println("Enter the length, width then height of the classroom");
+        int length = Integer.parseInt(sa.next());
+        int width = Integer.parseInt(sa.next());
+        int height = Integer.parseInt(sa.next());
+        System.out.printf("The area of a room %s' wide and %s' long is: "  + (length * width) +" square feet.\n", width,length);
+        System.out.println("The perimeter is " + ((width * 2) + (length * 2)) + " linear feet.");
+        System.out.println("The volume is " + ((length * width *  height)/27) + " cubic yards.");
+
+//
+//        String all = "l 10 w 20 h 12";
+//        Scanner sa = new Scanner(all);
+//        sa.useDelimiter("\n");
+//        System.out.println(((sa.hasNextInt() * 2) + (sa.hasNextInt() * 2) + (sa.hasNextInt() * 0)));
+    }
+}
+
