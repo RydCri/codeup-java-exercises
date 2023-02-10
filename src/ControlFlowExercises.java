5afc9e2029b86ebb6ef756e1851b92461eb98c0f
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-10 11:12:50 -0600

    java loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,13 +1,44 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-//        int i = 5;
-//        while (i <= 15){
-//            System.out.println(i);
-//            i++;
-//        }
+        int i = 5;
+        while (i <= 15){
+            System.out.println(i);
+            i++;
+        }
 
+//            int y = 0;
+//            do {
+//                y += 2;
+//                System.out.println(y);
+//            }while (y < 1000000);
+
+//                int y = 100;
+//                do {
+//                    y -= 5;
+//                    System.out.println(y);
+//                }while (y > -10);
+
+                long y = 2;
+                do {
+                    System.out.println(y);
+                    y *= y;
+                }while (y < 1000000);
+
+
+        for (int l = 1; l <= 100; l++) {
+            if (l % 15 == 0) {
+                System.out.printf("%d fizzBuzz\n",l);
+            } else if (l % 3 == 0) {
+                System.out.printf("%d fizz\n",l);
+            } else if (l % 5 == 0) {
+                System.out.printf("%d buzz\n",l);
+
+            } else {
+                System.out.println(l);
+            }
 
         }
     }
+}
 
