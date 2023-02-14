9d3aad431d62197a73e3b3b59a1c739fab901ab9
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-14 16:06:48 -0600

    class Person uses two constructors for created person objects and a sayHello method that passes the person's name with a greeting

---
+++ src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    public String name;
+    public Person() {
+        // default constructor
+        System.out.println("A person is being constructed");
+    }
+       public Person(String name) {
+        // calling method with passed String will add String to Person.name field
+            this.name = name;
+        }
+
+    public String sayHello() {
+        return String.format("%s says hello!", this.name);
+    }
+
+
+    public static void main(String[] args) {
+        Person p1 = new Person("Jim"); //will bypass default constructor
+        Person p2 = new Person();
+        p2.name = "Jom"; //bypasses naming method
+        System.out.println(p1.sayHello());
+        System.out.println(p2.sayHello());
+    }
+}
