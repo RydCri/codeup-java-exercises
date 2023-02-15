b4469dd72c39eb4f6006b47d613b89c5e10051b9
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-15 11:42:11 -0600

    Dish constructor is public, Dish instance variables are private, Adding Dish instance fields are encapsulated in the makeDish void method.

--- src/Dish.java
+++ src/Dish.java
@@ -1,10 +1,14 @@
 public class Dish {
-    public String nameOfDish;
-    public int costInCents;
-    public boolean wouldRecommend;
-
+    private String nameOfDish;
+    private int costInCents;
+    private boolean wouldRecommend;
+public void makeDish(String nameOfDish, int costInCents, boolean wouldRecommend) {
+    this.nameOfDish = nameOfDish;
+    this.costInCents = costInCents;
+    this.wouldRecommend = wouldRecommend;
+}
    public void printSummary(){
-   System.out.printf("Cost: %s\n Name: %s\n Recommended: %s\n",costInCents,nameOfDish,wouldRecommend);
+   System.out.printf("Name: %s\nCost: %s\nRecommended: %s\n|================|\n",nameOfDish,costInCents,wouldRecommend);
 }
     public void main(String[] args) {
 
