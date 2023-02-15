99e1f25a165a1e487daa902ccc45226ad1c747dc
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-15 13:38:56 -0600

    Instanced, constructed and printed Dish objects dish1 & dish2

--- src/DishTest.java
+++ src/DishTest.java
@@ -2,10 +2,12 @@ public class DishTest {
     public static void main(String[] args) {
     Dish dish1 = new Dish();
          dish1.makeDish("Spaghetti",2099,true);
+
     Dish dish2 = new Dish();
          dish2.makeDish("Macaroni",1599,false);
 
          System.out.println("|====Dishes====|\n");
+
             dish1.printSummary();
 
             dish2.printSummary();
