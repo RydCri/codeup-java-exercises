b7add53efe23d45b220c4203c14d91ccce040043
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-15 11:42:48 -0600

    Instanced, constructed and printed Dish objects dish3 and dish4

--- src/DishTest.java
+++ src/DishTest.java
@@ -1,16 +1,13 @@
 public class DishTest {
     public static void main(String[] args) {
     Dish dish1 = new Dish();
-         dish1.nameOfDish = "Spaghetti";
-         dish1.costInCents = 99;
-         dish1.wouldRecommend = true;
-
+         dish1.makeDish("Spaghetti",2099,true);
     Dish dish2 = new Dish();
-         dish2.nameOfDish = "Pepper Chicken";
-         dish2.costInCents = 150;
-         dish2.wouldRecommend = false;
+         dish2.makeDish("Macaroni",1599,false);
 
+         System.out.println("|====Dishes====|\n");
             dish1.printSummary();
+
             dish2.printSummary();
     }
 }
