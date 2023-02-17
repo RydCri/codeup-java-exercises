e07c4d3f2e31d77e9dfcda41bc3275f6cd88ceb3
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-17 15:13:59 -0600

    instants Square and Rectangle objects and returns fields through getArea and getPerimeter methods

---
+++ src/shapes/shapesTest.java
@@ -0,0 +1,16 @@
+package shapes;
+
+public class shapesTest{
+
+
+    public static void main(String[] args) {
+    Rectangle box1 = new Rectangle(4,5);
+    Square box2 = new Square(3);
+    box1.shapeTest();
+    box1.getPerimeter();
+    box1.getArea();
+    box2.shapeTest();
+    box2.getPerimeter();
+    box2.getArea();
+    }
+}
