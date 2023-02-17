1a9e818b413022001db68e6f44299fa1b81cbbe7
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-17 16:53:35 -0600

    init -practice classes

---
+++ src/Animals/Fish.java
@@ -0,0 +1,9 @@
+package Animals;
+
+public class Fish extends Animals{
+        public Fish(int numberOfLegs,String preferredClimate){
+            super(numberOfLegs,preferredClimate);
+        }
+    public static void main(String[] args) {
+    }
+}
