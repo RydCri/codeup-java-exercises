31cada89c8972dbd6fa2a0c9a205550b36a9dfce
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-17 16:50:04 -0600

    Added Override

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -5,6 +5,15 @@ package shapes;
             super(sides,sides);
         }
 
+//        @Override
+//            public void getArea(){
+//                System.out.println("Method overide");
+//                System.out.println(length * length);
+//            }
+//            public void getPerimeter(){
+//                System.out.println("Method overide");
+//                System.out.println(length * 4);
+//            }
         public static void main(String[] args) {
 
         }
