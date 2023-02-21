86b08d06a9629b038909fd1f3af0362550f85b85
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-21 14:42:36 -0600

    nln

--- src/shapes/shapesTest.java
+++ src/shapes/shapesTest.java
@@ -2,15 +2,13 @@ package shapes;
 
 public class shapesTest{
 
-
-    public static void main(String[] args) {
-    Rectangle box1 = new Rectangle(4,5);
-    Square box2 = new Square(5);
-    box1.shapeTest();
-    box1.getPerimeter();
-    box1.getArea();
-    box2.shapeTest();
-    box2.getPerimeter();
-    box2.getArea();
-    }
+//defunct
+//    public static void main(String[] args) {
+//    Rectangle box1 = new Rectangle(4,5);
+//    Square box2 = new Square(5);
+//    box1.getPerimeter();
+//    box1.getArea();
+//    box2.getPerimeter();
+//    box2.getArea();
+//    }
 }
