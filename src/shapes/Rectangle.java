bbd2602853676003ad95013908ea9d2ec30f378f
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-21 14:44:41 -0600

    Rectangle now extends Quadrilateral

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,29 +1,29 @@
 package shapes;
 
-class Rectangle {
-    protected int length;
-    protected int width;
-    public int area;
-    public int perimeter;
-public Rectangle(int length,int width){
-    this.length = length;
-    this.width = width;
-}
-public void getArea(){
-    this.area = this.width * this.length;
-        System.out.println("Area:\t" + area + "\n");
+
+class Rectangle extends Quadrilateral {
+public int length;
+public int width;
+    public Rectangle(int length, int width){
+        super(length,width);
     }
-    public void getPerimeter(){
-        this.perimeter = (this.width * 2) + (this.length * 2);
-        System.out.println("Perimeter:\t" + perimeter);
-    }
-    public void shapeTest(){
-    if(this.width == this.length){
-        System.out.printf("Square sides: %d\n",length);
-    }else{
-        System.out.printf("Rectangle length: %d, width: %d\n",length,width);
-    }
-    }
+
+//public void getArea(){
+//    this.area = this.width * this.length;
+//        System.out.println("Area:\t" + area + "\n");
+//    }
+
+//    public void getPerimeter(){
+//        this.perimeter = (this.width * 2) + (this.length * 2);
+//        System.out.println("Perimeter:\t" + perimeter);
+//    }
+//    public void shapeTest(){
+//    if(this.width == this.length){
+//        System.out.printf("Square sides: %d\n",length);
+//    }else{
+//        System.out.printf("Rectangle length: %d, width: %d\n",length,width);
+//    }
+//    }
 
     public static void main(String[] args) {
     }
