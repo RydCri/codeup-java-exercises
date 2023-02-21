9c2277121f1cc859d4339f2f4d1ee3b84632c9fd
RydCri <107228115+RydCri@users.noreply.github.com>
2023-02-21 14:46:13 -0600

    superclass Shape instances Square and Rectangle objects then calls Quad's methods

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -4,5 +4,15 @@ abstract public class Shape {
 
 
     public static void main(String[] args) {
+
+    Quadrilateral q1 = new Rectangle(3,4);
+    q1.shapeTest();
+    q1.getPerimeter();
+    q1.getArea();
+
+    Quadrilateral q2 = new Square(3);
+    q2.shapeTest();
+    q2.getPerimeter();
+    q2.getArea();
     }
 }
